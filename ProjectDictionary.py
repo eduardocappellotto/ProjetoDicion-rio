a5db3fd70adf0ca3cbf5aac5ee3974bfa4cf32e0
eduardocappellotto <51893328+eduardocappellotto@users.noreply.github.com>
2019-09-17 20:23:10 -0300

    Update ProjectDictionary.py
    Ver 0.2 - Fixing bugs

--- ProjectDictionary.py
+++ ProjectDictionary.py
@@ -9,12 +9,31 @@ def dicionario(palavra):
     if palavra in data:             #Certifica que a palavra está no dicionário
         return data[palavra]
 
-    elif len(get_close_matches(palavra , data.keys())) > 0:
-        return "Did you mean %s instead?" % get_close_matches(palavra , data.keys())[0]
-        
+    elif palavra.title() in data:
+        return data[palavra.title()]
 
-    else: return "A palavra não existe, verifique a inserção"
+    elif palavra.upper() in data:
+        return data[palavra.upper()]
+
+    elif len(get_close_matches(palavra , data.keys())) > 0:
+        yn = input( "Did you mean %s instead? (Y - Yes // N - No) " % get_close_matches(palavra , data.keys())[0] )
+        
+        if   yn == "Y":
+            return data[get_close_matches(palavra, data.keys())[0]]
+        
+        elif yn == "N":
+            return "We didn't understand your entry."
+
+    else:
+        return "The word doesnt exist. Please double check it" 
+    
 
 palavra = input("Insira a palavra em inglês: ")
 
-print( dicionario(palavra))
+output = dicionario(palavra)
+
+if type(output) == list :
+    for item in output:
+        print(item)
+else:
+    print(output)
